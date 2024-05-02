309fab62a93162d6681443c7f69da69ffc504667
Krish Acme <acmekrish@gmail.com>
2024-05-02 16:07:45 +0100

    movies

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -3,5 +3,4 @@ public class Movie
     public string Title { get; set; }
     public string Year { get; set; }
     public string Poster { get; set; }
-    // ... other movie details as per the API response
 }
--- Program.cs
+++ Program.cs
@@ -12,12 +12,12 @@ public class Program
                 .AllowAnyHeader();
         });
     });
-        // Add services to the container.
+        
         builder.Services.AddControllers();
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
+       
         app.UseHttpsRedirection();
         app.UseAuthorization();
         app.UseCors("AllowReactApp");
